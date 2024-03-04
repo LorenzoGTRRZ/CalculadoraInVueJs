bc36283eafe538d468d8a1b49cb681c9fbd61236
Lorenzo Gutierrez <lorenzoplaypereira@gmail.com>
2024-03-04 13:40:06 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Calculator from './components/Calculator.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+    <Calculator />
+  </div>
+</template>
+
+<style scoped>
+</style>
